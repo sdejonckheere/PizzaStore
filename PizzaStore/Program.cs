d8b6db89051ca73d34a3bd0f790621341e913dca
sdejonckheere <sdejonckheere@live.nl>
2015-03-19 19:21:17 +0100

    Commit 1

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -1,22 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace PizzaStore
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadLine();
+        
         }
     }
 }
